f4644a1c499343b60c5d2cfdfa8861b44dad2eaa
Luana Correia <luacooli@gmail.com>
2024-02-18 18:26:14 -0300

    feat: creating a service

--- index.js
+++ index.js
@@ -1,88 +1,63 @@
 const express = require('express')
 const app = express()
+
 const port = 3000
+
+const UsersService = require('./users.service')
+const users = new UsersService()
 
 app.use(express.json()) // body-parsing middleware for parsing application/json and populate req.body
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
-  res.json({
-    ok: true,
-  })
 })
 
-const users = [
-  {
-    _id: 0,
-    name: 'José',
-    email: 'jose@email.com',
-    commits: 150,
-    birthdate: '1990-06-01',
-    status: true,
-  },
-  {
-    _id: 1,
-    name: 'Maria',
-    email: 'maria@email.com',
-    commits: 100,
-    birthdate: '1999-07-12',
-    status: true,
-  },
-  {
-    _id: 2,
-    name: 'Pedro',
-    email: 'pedro@email.com',
-    commits: 80,
-    birthdate: '1986-03-21',
-    status: false,
-  },
-]
-
 app.get('/users', (req, res) => {
-  res.status(200).json(users)
+  res.status(200).json(users.getUsers())
 })
 
 app.get('/users/:userId', (req, res) => {
-  res.status(200).json(users[req.params.userId])
+  const userId = parseInt(req.params.userId)
+
+  const user = users.getUserById(userId)
+
+  if (user) {
+    res.status(200).json(user)
+  } else {
+    res.status(404).send('Usuário não encontrado')
+  }
 })
 
 app.post('/users', (req, res) => {
   const newUser = req.body
 
-  users.push(newUser)
+  const user = users.createUser(newUser)
 
-  res.status(201).json(users)
+  res.status(201).json(user)
 })
 
 app.put('/users/:userId', (req, res) => {
-  const userId = req.params.userId
+  const userId = parseInt(req.params.userId)
+  const userData = req.body
 
-  if (!users[userId]) {
-    res
-      .status(400)
-      .send(`User id doens't exist - user_Id: ${req.params.userId}`)
+  const user = users.updateUser(userId, userData)
+
+  if (user) {
+    res.status(200).json(user)
   } else {
-    users[userId] = {
-      _id: userId,
-      name: users[userId].name,
-      email: users[userId].email,
-      commits: users[userId].commits + 10,
-      birthdate: users[userId].birthdate,
-      status: users[userId].status,
-    }
-
     res.json(users)
   }
 })
 
 app.delete('/users/:userId', (req, res) => {
-  const userId = req.params.userId
+  const userId = parseInt(req.params.userId)
 
-  if (!users[userId]) {
-    res.status(400).send(`User id doens't exist - userId: ${req.params.userId}`)
+  const usersFiltered = users.deleteUser(userId)
+
+  if (usersFiltered) {
+    res.status(200).send('Usuário deletado com sucesso!')
   } else {
-    usersFiltered = users.filter((user) => user.id != userId)
-    res.status(200).json(usersFiltered)
+    res.status(404).send('Usuário não encontrado')
   }
 })
 
--- users.service.js
+++ users.service.js
@@ -1,11 +1,74 @@
 class UsersService {
-  createUser() {}
+  constructor() {
+    this.users = [
+      {
+        _id: 0,
+        name: 'José',
+        email: 'jose@email.com',
+        commits: 150,
+        birthdate: '1990-06-01',
+        status: true,
+      },
+      {
+        _id: 1,
+        name: 'Maria',
+        email: 'maria@email.com',
+        commits: 100,
+        birthdate: '1999-07-12',
+        status: true,
+      },
+      {
+        _id: 2,
+        name: 'Pedro',
+        email: 'pedro@email.com',
+        commits: 80,
+        birthdate: '1986-03-21',
+        status: false,
+      },
+    ]
+  }
 
-  updateUser() {}
+  getUsers() {
+    return this.users
+  }
 
-  getUsers() {}
-  
-  getUserById() {}
+  getUserById(userId) {
+    const user = this.users.find((user) => {
+      user._id === userId
+    })
 
-  deleteUser() {}
+    return user
+  }
+
+  createUser(newUser) {
+    newUser._id = this.users.length + 1
+
+    this.users.push(newUser)
+
+    return newUser
+  }
+
+  updateUser(userId, body) {
+    const userIndex = this.users.findIndex((user) => userId === user._id)
+
+    if (userIndex !== -1) {
+      this.users[userIndex] = { ...this.users[userIndex], ...body }
+
+      return this.users[userIndex]
+    } else {
+      throw new Error('Usuário não encontrado')
+    }
+  }
+
+  deleteUser(userId) {
+    const userIndex = this.users.findIndex((user) => userId === user._id)
+
+    if (userIndex !== -1) {
+      return this.users.filter((user) => user.id !== userId)
+    } else {
+      throw new Error('Usuário não encontrado')
+    }
+  }
 }
+
+module.exports = UsersService
